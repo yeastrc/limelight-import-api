d84755e20622eda2203cbedf26c55e43d600f64e
Michael Riffle <mriffle@uw.edu>
2018-06-08 16:26:02 -0700

    Refactored to use limelight instead of emozi in java code and paths

---
+++ src_create_import_file_from_java_objects/org/yeastrc/limelight/limelight_import/create_import_file_from_java_objects/main/CreateImportFileFromJavaObjectsMain.java
@@ -0,0 +1,83 @@
+package org.yeastrc.limelight.limelight_import.create_import_file_from_java_objects.main;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.yeastrc.limelight.limelight_import.api.xml_dto.limelightInput;
+
+
+/**
+ * 
+ *
+ */
+public class CreateImportFileFromJavaObjectsMain {
+	
+//	private static final Logger log = Logger.getLogger(CreateImportFileFromJavaObjectsMain.class);
+
+	// private constructor
+	private CreateImportFileFromJavaObjectsMain() { }
+	
+	public static CreateImportFileFromJavaObjectsMain getInstance() { 
+		return new CreateImportFileFromJavaObjectsMain(); 
+	}
+	
+	
+	/**
+	 * @param outputFile
+	 * @param proxlInput
+	 * @throws Exception
+	 */
+	public void createImportFileFromJavaObjectsMain( File outputFile, limelightInput limelightInput ) throws Exception {
+		
+
+		OutputStream outputStream = null;
+		
+		try {
+
+			outputStream = new FileOutputStream(outputFile);
+			
+			createImportFileFromJavaObjectsMain( outputStream, limelightInput );
+		} finally {
+			
+			if ( outputStream != null ) {
+				
+				outputStream.close();
+			}
+		}
+	}
+	
+	
+
+	/**
+	 * @param outputStream
+	 * @param proxlInput
+	 * @throws Exception
+	 */
+	public void createImportFileFromJavaObjectsMain( OutputStream outputStream, limelightInput limelightInput ) throws Exception {
+		
+		JAXBContext jaxbContext = JAXBContext.newInstance( limelightInput.class );
+
+		Marshaller marshaller = jaxbContext.createMarshaller();
+
+
+		marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+
+//		marshaller.setProperty( Marshaller.JAXB_ENCODING, CommonConstants.FILE_ENCODING );
+
+
+		
+		try {
+			
+			marshaller.marshal( limelightInput, outputStream );
+			
+		} finally {
+			
+		}
+		
+		
+	}
+}
